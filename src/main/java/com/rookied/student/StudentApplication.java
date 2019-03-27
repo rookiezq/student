cf017e47d257470f583efc2bef4d72367535c8e7
Demon <18112358325@163.com>
2019-03-27 15:11:57 +0800

    创建项目

---
+++ src/main/java/com/rookied/student/StudentApplication.java
@@ -0,0 +1,13 @@
+package com.rookied.student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentApplication.class, args);
+    }
+
+}
