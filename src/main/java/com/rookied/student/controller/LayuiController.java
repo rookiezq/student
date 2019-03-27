13549d63b62e03ce5ef1dcb582c681aad43f2bcc
Demon <18112358325@163.com>
2019-03-27 15:54:12 +0800

    首页

---
+++ src/main/java/com/rookied/student/controller/LayuiController.java
@@ -0,0 +1,19 @@
+package com.rookied.student.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @desciption:
+ * @author: Demon
+ * @version: 1.0 2019-03-27 15:41
+ **/
+@Controller
+@RequestMapping("/layui")
+public class LayuiController {
+
+    @RequestMapping("/index")
+    public String demo(){
+        return "layui/index";
+    }
+}
